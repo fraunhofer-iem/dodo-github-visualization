8df0fe32942c097ba948501f28fc3efa4de3413f
Jan-Niclas Strüwer <j.n.struewer@gmail.com>
2021-10-20 16:53:11 +0200

    started pagination

--- pages/api/projects.ts
+++ pages/api/projects.ts
@@ -1,4 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next"
+import { PAGE_SIZE_LIMIT, Pagination } from "../../util/commonApiTypes"
 import projects from "../../util/data/projects.json"
 
 export type Project = {
@@ -11,5 +12,27 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Project[]>,
 ) {
+  const pagination = getPagination(req);
   res.status(200).json(projects)
+}
+
+const getPagination = (req: NextApiRequest): Pagination => {
+  if(hasPageNumber(req)){
+    if(hasValidPageSize(req)){
+      return {pageNumber: req.query.pageNumber, pageSize: req.query.pageSize}
+    } else {
+      return {pageNumber: req.query.pageNumber, pageSize: PAGE_SIZE_LIMIT}
+    }
+  } else {
+    return {pageSize: PAGE_SIZE_LIMIT, pageNumber: 1}
+  }
+}
+
+const hasPageNumber = (req: NextApiRequest) => {
+  return req.hasOwnProperty('pageNumber') ;
+}
+
+
+const hasValidPageSize = (req: NextApiRequest) => {
+  return (req.hasOwnProperty('pageSize') && ObjectpageSize > PAGE_SIZE_LIMIT)
 }
---
+++ util/commonApiTypes.ts
@@ -0,0 +1,6 @@
+export const PAGE_SIZE_LIMIT = 50;
+
+export type Pagination = {
+    pageSize: number
+    pageNumber: number
+}
