8ad29da269091a5764df70825e02074061588fb1
Jan-Niclas Strüwer <jan-niclas.struewer@iem.fraunhofer.de>
2021-10-26 10:11:55 +0200

    updated pagination behavior to take lower bound (1) into account. and actually check the page size limit in the validation checks

--- util/api/pagination.ts
+++ util/api/pagination.ts
@@ -1,35 +1,41 @@
-import { NextApiRequestQuery } from "next/dist/server/api-utils";
+import { NextApiRequestQuery } from "next/dist/server/api-utils"
 
-export const PAGE_SIZE_LIMIT = 50;
+export const PAGE_SIZE_LIMIT = 50
 
 export type Pagination = {
-    pageSize: number
-    pageNumber: number
+  pageSize: number
+  pageNumber: number
 }
 
 export const getPagination = (query: NextApiRequestQuery): Pagination => {
-    if (hasValidPageNumber(query)) {
-      if (hasValidPageSize(query)) {
-        return { pageNumber: +query.pageNumber, pageSize: +query.pageSize }
-      } else {
-        return { pageNumber: +query.pageNumber, pageSize: PAGE_SIZE_LIMIT }
-      }
+  if (hasValidPageNumber(query)) {
+    if (hasValidPageSize(query)) {
+      return { pageNumber: +query.pageNumber, pageSize: +query.pageSize }
     } else {
-      return { pageSize: PAGE_SIZE_LIMIT, pageNumber: 1 }
+      return { pageNumber: +query.pageNumber, pageSize: PAGE_SIZE_LIMIT }
     }
+  } else {
+    return { pageSize: PAGE_SIZE_LIMIT, pageNumber: 1 }
   }
-  
-  const hasValidPageNumber = (query: NextApiRequestQuery) => {
-    return query.hasOwnProperty("pageNumber") && isNumber(query["pageNumber"])
+}
+
+const hasValidPageNumber = (query: NextApiRequestQuery) => {
+  return (
+    query.hasOwnProperty("pageNumber") &&
+    isNumber(query["pageNumber"]) &&
+    +query["pageNumber"] > 0
+  )
+}
+
+const hasValidPageSize = (query: NextApiRequestQuery) => {
+  if (query.hasOwnProperty("pageSize") && isNumber(query["pageSize"])) {
+    const pageSize = +query["pageSize"]
+    // check if pageSize is in a valid range
+    return pageSize > 0 && pageSize <= PAGE_SIZE_LIMIT
   }
-  
-  const hasValidPageSize = (query: NextApiRequestQuery) => {
-    if (query.hasOwnProperty("pageSize")) {
-      return isNumber(query["pageSize"]);
-    }
-    return false
-  }
-  
-  const isNumber = (numVal: string | string[]) => {
-    return typeof numVal == "string" && !isNaN(+numVal)
-  }
+  return false
+}
+
+const isNumber = (numVal: string | string[]) => {
+  return typeof numVal == "string" && !isNaN(+numVal)
+}
