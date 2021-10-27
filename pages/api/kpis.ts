77d61ab49a3673aa349ea9217003f68f56bb8b10
Jan-Niclas Strüwer <jan-niclas.struewer@iem.fraunhofer.de>
2021-10-27 10:14:44 +0200

    fixed wrong import path

--- pages/api/kpis.ts
+++ pages/api/kpis.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next"
-import { KpiDetail } from "./project/[pid]/kpi/[kid]"
+import { KpiDetail } from "./projects/[pid]/kpis/[kid]"
 
 export default function handler(
   req: NextApiRequest,
